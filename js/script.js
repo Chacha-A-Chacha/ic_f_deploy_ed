766742ef3d14633d38702dbffecf27af8e7ef9cf
Chacha-A-Chacha <calex2607@gmail.com>
2023-01-20 01:33:41 +0300

    nav bar changes
    nav-bar adjustment

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
- //Js
- document.querySelector(".mobile-btn").addEventListener("click", function () {
-    document.querySelector(".menu").classList.toggle("active");
+window.addEventListener('DOMContentLoaded', (event) => {
+    document.querySelectorAll(".mobile-btn").forEach(el => {
+        el.addEventListener('click',function (e) {
+            document.querySelector(".menu").classList.toggle("active")
+        });
     });
+});
